7831ffd03bdbb504542c401c1014fc61824cdf30
KroNton <kyrlosfekry@gmail.com>
2025-06-14 00:44:32 +0300

    Refactor standard deviation calculation for improved readability and consistency

--- 02/standard_deviation.js
+++ 02/standard_deviation.js
@@ -1,26 +1,25 @@
 
-var sd = mu = n = a = b =0;
-var x=[];
+var sd = mu = n = a = b = 0;
+var x = [];
 
 n = Number(prompt("enter N value"));
 
-x.length=n
-
-for (var i = 0; i <x.length; i++) {
-    x[i]=Number(prompt("enter element value"));
-    mu+=x[i];
-
-}
-
-mu=mu/n;
+x.length = n
 
 for (var i = 0; i < x.length; i++) {
-   a+=Math.pow(x[i]-mu,2);
+    x[i] = Number(prompt("enter element value"));
+    mu += x[i];
 }
 
-b=a/n;
+mu = mu / n;
 
-sd=Math.sqrt(b);
+for (var i = 0; i < x.length; i++) {
+    a += Math.pow(x[i] - mu, 2);
+}
+
+b = a / n;
+
+sd = Math.sqrt(b);
 
 
 console.log(sd);
